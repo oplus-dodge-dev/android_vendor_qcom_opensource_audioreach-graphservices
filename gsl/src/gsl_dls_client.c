02d1f15b25ec9cd1ec51b559beb0c6e959ce2db4
dianlujitao <dianlujitao@lineageos.org>
2025-03-26 11:37:49 +0800

    gsl: ATS Data Logging Service is not supported

--- gsl/src/gsl_dls_client.c
+++ gsl/src/gsl_dls_client.c
@@ -33,7 +33,7 @@ struct gsl_dls_client_ctxt dls_client_ctxt;
 
 int32_t gsl_dls_client_is_feature_supported()
 {
-    return AR_EOK;
+    return AR_EUNSUPPORTED;
 }
 
 static int32_t gsl_dls_client_register_deregister_commit_log_buffer_event(enum gsl_dls_commit_log_buffer_register_enum_t event_register_flag)
